5257e2431fe5e013d530d978c53978ef47c56a77
Vandit <vanditkalathiya18@gmail.com>
2022-12-24 14:02:25 +0530

    42

---
+++ 4.2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("\n\t\t\tMALTIPLICATION TABLE(1 TO 7)");
+    printf("\n---------------------------------------------------------------");
+
+    for(a=1;a<=10;a++)
+    {
+        printf("\n");
+
+        for(b=1;b<=7;b++)
+        {
+            printf("\t%d",a*b);
+        }
+    }
+    printf("\n---------------------------------------------------------------");
+    printf("\nThis program is made by VANDIT KALATHIYA");
+}
